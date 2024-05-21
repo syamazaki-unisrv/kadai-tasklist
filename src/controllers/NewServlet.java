8d84c2d6c573c16d738fcb238367abba8aa1ea27
shintarou.yamazaki <syamazaki@unisrv.jp>
2024-05-21 11:07:43 +0900

    Index/NewServletの修正

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,7 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
 
-import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,19 +10,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.TaskList;
-import utils.DBUtil;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class NewServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public NewServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -33,19 +30,14 @@ public class IndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        EntityManager em = DBUtil.createEntityManager();
+     // CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
 
-        List<TaskList> tasklists = em.createNamedQuery("getAllTaskLists", TaskList.class).getResultList();
+        // おまじないとしてのインスタンスを生成
+        request.setAttribute("TaskList", new TaskList());
 
-        em.close();
-
-        request.setAttribute("tasklists", tasklists);
-
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasklists/index.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasklists/new.jsp");
         rd.forward(request, response);
-
-
     }
 
 }
--- src/models/TaskList.java
+++ src/models/TaskList.java
@@ -7,9 +7,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTaskLists",
+        query = "SELECT m FROM TaskList AS m ORDER BY m.id DESC"
+    )
+})
 @Table(name = "tasklists")
 public class TaskList {
     @Id
